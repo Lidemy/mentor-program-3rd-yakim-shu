2fd271167e628003af8aac955a34f221e31b71e1
yakim shu <yakim-shu@users.noreply.github.com>
2019-05-12 13:29:39 +0800

    Update week3

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,11 +1,10 @@
 function add(a, b) {
-  let temp = 0;
   let temp10 = 0;
   let ans = '';
   const Max = a.length > b.length ? a.length : b.length;
   for (let i = 1; i <= Max; i += 1) {
     // 從後往前取數字，空位補 0
-    temp = (Number(a[a.length - i]) || 0) + (Number(b[b.length - i]) || 0) + temp10;
+    let temp = (Number(a[a.length - i]) || 0) + (Number(b[b.length - i]) || 0) + temp10;
     temp10 = temp > 9 ? 1 : 0;
     temp %= 10;
     ans = temp + ans;

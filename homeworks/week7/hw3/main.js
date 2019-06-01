5ba7132853da55550b48a86c4f08627523c02770
yakim shu <yakim-shu@users.noreply.github.com>
2019-06-01 13:30:44 +0800

    Update hw3 of wrap into object

--- homeworks/week7/hw3/main.js
+++ homeworks/week7/hw3/main.js
@@ -1,110 +1,115 @@
 const inputResultTop = document.querySelector('.row__result span');
 const inputResult = document.querySelector('.row__result strong');
 const calculator = document.querySelector('.calculator');
-const calcAll = ['+', '-', '*', '/'];
 const numFixed = 5; // 小數點後 5 位直接刪掉
-let numOrigin = '';
-let numResult = '';
-let calcSign = '';
-let isCalculated = false;
-let isFinish = false;
 
-function showError() {
-  calculator.classList.add('animated', 'shake', 'fast');
-}
-
-// 計算 function
-function calculatoring(sign, first, sec) {
-  switch (sign) {
-    case '+':
-      // console.log()
-      return first + sec;
-    case '-':
-      return first - sec;
-    case '*':
-      return first * sec;
-    case '/':
-      if (sec === '0') {
-        showError();
-        return ['任何數除以 0 => 無意義', '🧞‍'];
-      }
-      return first / sec;
-    default:
-      break;
-  }
-  return false;
-}
+const numManage = {
+  numOrigin: '',
+  numResult: '',
+};
+const signManage = {
+  calcSignAll: ['+', '-', '*', '/'],
+  calcSignCurrent: '',
+};
+const stateManage = {
+  isCalculated: false,
+  isFinish: false,
+  isClickCalc: false,
+};
+const funManage = {
+  dataProcess: value => ((value[0] === '0' && value.length !== 1) ? value.substr(1) : value),
+  calc: (subValue, mainValue) => {
+    const result = [funManage.dataProcess(subValue), funManage.dataProcess(mainValue)];
+    if (stateManage.isFinish && mainValue % 1) result[1] = funManage.isFloat(mainValue);
+    [inputResultTop.innerText, inputResult.innerText] = result;
+  },
+  isFloat: (mainValue) => {
+    const num = mainValue.toString();
+    if (num.split('.')[1].length < numFixed) return mainValue;
+    return Number(num).toFixed(numFixed);
+  },
+  doCalc: (sign, first, sec) => {
+    switch (sign) {
+      case '+':
+        return first + sec;
+      case '-':
+        return first - sec;
+      case '*':
+        return first * sec;
+      case '/':
+        if (sec === 0) {
+          return this.showError('無意義 🧞‍');
+        }
+        return first / sec;
+      default:
+        break;
+    }
+    return false;
+  },
+};
 
 // click - 歸零 (AC)
-function clickReset() {
-  isCalculated = false;
-  isFinish = false;
-  numOrigin = '';
-  numResult = '';
+function clickReset(msg) {
+  if (msg) return msg;
+  [stateManage.isCalculated, stateManage.isFinish] = [false, false];
+  [numManage.numOrigin, numManage.numResult] = ['', ''];
   return ['0', '0'];
 }
-// click - 加、減、乘、除 (+、-、*、/)
-function clickCalc(sign) {
-  isCalculated = true;
-  calcSign = sign;
-  if (numResult) return [numResult + sign, sign];
-  return [numOrigin, sign];
+
+function showError(msg) {
+  calculator.classList.add('shake');
+  return clickReset(msg);
 }
+// show 結果
+function showResult(arr) {
+  // 判斷是數字鍵、還是符號
+  if (!arr && numManage.numResult) showResult(showError()); // 數字鍵
+  else if (!arr) funManage.calc(numManage.numOrigin, numManage.numOrigin);
+  else funManage.calc(arr[0], arr[1]); // 其他符號
+}
+
 // click - 等於 (=)
 function clickResult() {
-  isCalculated = false;
-  isFinish = true;
-  const arr = numOrigin.split(calcSign);
+  stateManage.isCalculated = false;
+  stateManage.isFinish = true;
+  const arr = numManage.numOrigin.split(signManage.calcSignCurrent);
   const numFir = Number(arr[0]);
   const numSec = Number(arr[1].substring(0, arr[1].length - 1));
-  numResult = calculatoring(calcSign, numFir, numSec);
-  return [numOrigin, numResult];
+  numManage.numResult = funManage.doCalc(signManage.calcSignCurrent, numFir, numSec);
+  return [numManage.numOrigin, numManage.numResult];
+}
+
+// click - 加、減、乘、除 (+、-、*、/)
+function clickCalc(sign) {
+  signManage.calcSignCurrent = sign;
+  if (numManage.numResult) numManage.numOrigin = numManage.numResult + sign;
+  if (stateManage.isCalculated) return showError(['0', '哎呀幫您歸零 😘']);
+  stateManage.isCalculated = true;
+  return [numManage.numOrigin, sign];
 }
 
 // 已有運算符號的處理，只顯示後來的數字
 function showMainNum(value) {
-  if (numResult) {
-    numOrigin = numResult + calcSign + value;
-    numResult = '';
-    isCalculated = true;
+  if (numManage.numResult) {
+    numManage.numOrigin = numManage.numResult + signManage.calcSignCurrent + value;
+    numManage.numResult = '';
+    stateManage.isCalculated = true;
   }
-  const index = numOrigin.indexOf(calcSign);
-  return [numOrigin, numOrigin.substr(index + 1)];
-}
-
-// 刪除多位數 float
-function isFloat(mainValue) {
-  const num = mainValue.toString();
-  if (num.split('.')[1].length < numFixed) return mainValue;
-  return Number(num).toFixed(numFixed);
-}
-
-// show 結果
-function showResult(arr) {
-  function calc(subValue, mainValue) {
-    const result = [];
-    result[0] = (subValue[0] === '0' && subValue.length !== 1) ? subValue.substr(1) : subValue;
-    result[1] = (mainValue[0] === '0' && mainValue.length !== 1) ? mainValue.substr(1) : mainValue;
-    if (isFinish && mainValue % 1) result[1] = isFloat(mainValue); // 小數點太多位數，進行刪減
-    [inputResultTop.innerText, inputResult.innerText] = result;
-  }
-
-  // 判斷是數字鍵、還是符號
-  if (!arr) calc(numOrigin, numOrigin); // 數字鍵
-  else calc(arr[0], arr[1]); // 其他符號
+  const index = numManage.numOrigin.indexOf(signManage.calcSignCurrent);
+  return [numManage.numOrigin, numManage.numOrigin.substr(index + 1)];
 }
 
 calculator.addEventListener('click', (e) => {
+  calculator.classList.remove('shake');
   const value = e.target.getAttribute('data-value');
-  const isClickCalc = calcAll.some(x => value === x);
+  stateManage.isClickCalc = signManage.calcSignAll.some(x => value === x);
   if (value === null) return;
-  calculator.classList.remove('animated', 'shake', 'fast');
 
-  numOrigin += value;
+  numManage.numOrigin += value;
 
   if (value === 'AC') showResult(clickReset());
-  else if (isClickCalc) showResult(clickCalc(value));
+  else if (stateManage.isClickCalc) showResult(clickCalc(value));
   else if (value === '=') showResult(clickResult());
-  else if (isCalculated) showResult(showMainNum(value));
+  else if (stateManage.isCalculated) showResult(showMainNum(value));
   else showResult();
 });

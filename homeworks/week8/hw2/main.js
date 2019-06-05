38decd12ba482b7f4e70c5168ab642ce407b60b3
yakim shu <yakim-shu@users.noreply.github.com>
2019-06-05 12:52:27 +0800

    Refactoring hw2

--- homeworks/week8/hw2/main.js
+++ homeworks/week8/hw2/main.js
@@ -2,57 +2,71 @@
 const request = new XMLHttpRequest();
 const dq = element => document.querySelector(element);
 const postGroup = dq('.comment__body');
-const url = 'https://lidemy-book-store.herokuapp.com/posts';
-const numList = 20; // 20 item for 1 page
+const baseUrl = 'https://lidemy-book-store.herokuapp.com/posts';
+const numList = 20; // 20 Msg for 1 page
 let postPage = 1;
-let latestId = 0;
-let lastPage = true;
-let firstPage = false;
 
-// prev、next 按鈕控制
-function navControl(currentId) {
-  if (latestId < currentId) latestId = currentId;
-  lastPage = (latestId === currentId);
-  firstPage = (currentId <= numList);
-  dq('.next').classList = (lastPage) ? 'btn next not-allowed' : 'btn next';
-  dq('.prev').classList = (firstPage) ? 'btn prev not-allowed' : 'btn prev';
-}
+// 拿資料、渲染畫面
+function RenderHtml() {
+  let latestId = 0;
+  let lastPage = true;
+  let firstPage = false;
 
-// 插入一筆留言
-function addNewPost(msg, i) {
-  const post = document.createElement('li');
-  post.innerHTML = `<span>${i} : ${msg}</span>`;
-  postGroup.prepend(post);
+  Object.defineProperty(this, 'banNext', { get: () => lastPage });
+  Object.defineProperty(this, 'banPrev', { get: () => firstPage });
+
+  // prev、next 按鈕控制
+  this.navControl = (currentId) => {
+    if (latestId < currentId) latestId = currentId;
+    lastPage = (latestId === currentId);
+    firstPage = (currentId <= numList);
+    dq('.next').classList = (lastPage) ? 'btn next not-allowed' : 'btn next';
+    dq('.prev').classList = (firstPage) ? 'btn prev not-allowed' : 'btn prev';
+  };
+
+  // 插入一筆留言
+  this.addNewPost = (msg, i) => {
+    const post = document.createElement('li');
+    post.innerHTML = `<span>${i} : ${msg}</span>`;
+    postGroup.prepend(post);
+  };
+
+  // 跑所有資料
+  this.getAllPost = (data) => {
+    for (let i = data.length - 1; i >= 0; i -= 1) {
+      this.addNewPost(data[i].content, data[i].id);
+    }
+    postGroup.classList.add('expand');
+    this.navControl(data[0].id);
+  };
 }
-// 跑所有資料
-function getAllPost(data) {
-  for (let i = data.length - 1; i >= 0; i -= 1) {
-    addNewPost(data[i].content, data[i].id);
-  }
-  postGroup.classList.add('expand');
-  navControl(data[0].id);
-}
+const render = new RenderHtml();
 
 // data - GET
-const getObj = {
-  method: 'GET',
-  url: `${url}?_limit=20&_sort=id&_order=desc&_page=1`,
-  load: json => getAllPost(json),
-};
+function getObj(page) {
+  return {
+    method: 'GET',
+    url: `${baseUrl}?_limit=20&_sort=id&_order=desc&_page=${page}`,
+    load: json => render.getAllPost(json),
+  };
+}
 
 // data - POST
-const postObj = {
-  method: 'POST',
-  url: 'https://lidemy-book-store.herokuapp.com/posts',
-  msg: '',
-  load: json => addNewPost(json.content),
-};
+function postObj(msg) {
+  return {
+    method: 'POST',
+    url: baseUrl,
+    content: msg,
+    load: json => render.addNewPost(json.content),
+  };
+}
 
 // 發送 request
 function sendRequest(obj) {
   request.open(obj.method, obj.url);
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  request.send(obj.msg);
+  request.send(obj.content);
+
   request.onload = () => {
     const status = request.status;
     if (status >= 200 && status < 300) {
@@ -64,36 +78,37 @@ function sendRequest(obj) {
   };
 }
 
-// 檢查輸入框是否有內容
-function checkInput() {
-  const msg = dq('.input').value;
-  if (msg === '') return;
-  dq('.input').value = '';
-  postObj.msg = `content=${msg}`;
-  sendRequest(postObj);
-}
+// 使用者操作（ 發送訊息、更新前後頁 ）
+function UserOperater() {
+  // 檢查輸入框是否有內容
+  this.checkInput = () => {
+    const msg = dq('.input').value;
+    if (msg === '') return;
+    dq('.input').value = '';
+    sendRequest(postObj(`content=${msg}`));
+  };
 
-// 更新前、後頁留言
-function updatePage(e, page) {
-  e.preventDefault();
-  postPage = (page === 'next') ? postPage -= 1 : postPage += 1;
-  getObj.url = `${url}?_limit=${numList}&_sort=id&_order=desc&_page=${postPage}`;
-  postGroup.innerHTML = '';
-  sendRequest(getObj);
+  // 更新前、後頁
+  this.updatePage = (sign) => {
+    postPage = (sign === 'next') ? postPage -= 1 : postPage += 1;
+    postGroup.innerHTML = '';
+    sendRequest(getObj(postPage));
+  };
 }
+const user = new UserOperater();
 
-sendRequest(getObj);
+sendRequest(getObj(1));
 
 // click 事件綁定
 window.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'BUTTON') checkInput();
-  if (e.target.getAttribute('data-btn') === 'next' && !lastPage) {
-    updatePage(e, 'next');
-  } else if (e.target.getAttribute('data-btn') === 'prev' && !firstPage) {
-    updatePage(e, 'prev');
-  }
+  e.preventDefault();
+  const target = e.target.innerText;
+  if (target === '送出') user.checkInput();
+  else if (target === 'Next' && !render.banNext) user.updatePage('next');
+  else if (target === 'Prev' && !render.banPrev) user.updatePage('prev');
 });
+
 // 輸入框按 Enter 也可以送出
 dq('.input').addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') checkInput();
+  if (e.key === 'Enter') user.checkInput();
 });
